dcb2f2fce098562199bedf7b404f5852729b035f
Bertrand Fan <bertrand@fan.net>
2020-10-07 01:45:06 -0700

    turn on WAL

--- knexfile.js
+++ knexfile.js
@@ -1,26 +1,26 @@
 module.exports = {
   dev: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './db/wheel.db'
-    },
-    pool: {
-      afterCreate: (conn, cb) => {
-        conn.run('PRAGMA foreign_keys = ON', cb);
-      }
-    }
+	client: 'sqlite3',
+	useNullAsDefault: true,
+	connection: {
+		filename: './db/wheel.db'
+	},
+	pool: {
+		afterCreate: (conn, cb) => {
+			conn.run('PRAGMA journal_mode=WAL; PRAGMA foreign_keys = ON', cb);
+		}
+	}
   },
   prod: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './db/wheel.db'
-    },
-    pool: {
-      afterCreate: (conn, cb) => {
-        conn.run('PRAGMA foreign_keys = ON', cb);
-      }
-    }
+	client: 'sqlite3',
+	useNullAsDefault: true,
+	connection: {
+		filename: './db/wheel.db'
+	},
+	pool: {
+		afterCreate: (conn, cb) => {
+			conn.run('PRAGMA journal_mode=WAL; PRAGMA foreign_keys = ON', cb);
+		}
+	}
   }
 };
